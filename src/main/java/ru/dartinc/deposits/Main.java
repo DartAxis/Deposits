3697d990689d8da9c9f2856100b217219d210448
lex <filatyev.a.a@gmail.com>
2024-01-29 13:18:40 +0300

    Добавлены ежегодные отчеты

--- src/main/java/ru/dartinc/deposits/Main.java
+++ src/main/java/ru/dartinc/deposits/Main.java
@@ -10,35 +10,52 @@ import java.time.LocalDate;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static final Double POPOLNENIE = 1000.0; //раз в полмесяца
-    public static final Double PREMIYA = 0.0;
-    public static final Double START = 0.0;
+    public static final Double POPOLNENIE = 75000.0; //раз в полмесяца
+    public static final Double PREMIYA = 450000.0;
+    public static final Double START = 800000.0;
+
+    public static final Double YEAR_PERCENT=15.0;
+    public static final Integer ACC=18;
+
 
 
     public static void main(String[] args) {
         clearReport();
         var mainAccount = new Account();
         mainAccount.setAmount(BigDecimal.valueOf(START));
-        var startDate = LocalDate.of(2024, 1, 1);
+        var startDate = LocalDate.of(2024, 1, 29);
         var endDate = LocalDate.of(2064, 12, 31);
         LocalDate nowDate = startDate;
         var name = 1;
         while (!nowDate.equals(endDate)) {
 
-            if (nowDate.getDayOfMonth() == 5 || nowDate.getDayOfMonth() == 20) {
+            if (nowDate.getDayOfMonth() == 10 || nowDate.getDayOfMonth() == 25) {
                 mainAccount.setAmount(mainAccount.getAmount().add(BigDecimal.valueOf(POPOLNENIE)));
             }
             if (nowDate.getDayOfMonth() == 25 && nowDate.getMonth().getValue() == 12) {
                 mainAccount.setAmount(mainAccount.getAmount().add(BigDecimal.valueOf(PREMIYA)));
             }
             if (nowDate.getDayOfMonth() % 7 == 0 && mainAccount.getAmount().doubleValue() >= 50000) {
-                if (mainAccount.getDeposits().size() < 12) {
-                    mainAccount.getDeposits().add(new Deposit(name, mainAccount.getAmount()
-                            , BigDecimal.valueOf(15)
-                            , nowDate
-                            , mainAccount));
-                    mainAccount.setAmount(BigDecimal.ZERO);
-                    name++;
+                if (mainAccount.getDeposits().size() < ACC) {
+                    while(mainAccount.getAmount().doubleValue()>0) {
+                        BigDecimal adding;
+                        if(mainAccount.getAmount().doubleValue()>1400000) {
+                            adding = BigDecimal.valueOf(1400000);
+                            mainAccount.getDeposits().add(new Deposit(name, adding
+                                    , BigDecimal.valueOf(YEAR_PERCENT)
+                                    , nowDate
+                                    , mainAccount));
+                            mainAccount.setAmount(mainAccount.getAmount().subtract(BigDecimal.valueOf(1400000)));
+                        } else {
+                            adding = mainAccount.getAmount();
+                            mainAccount.getDeposits().add(new Deposit(name, adding
+                                    , BigDecimal.valueOf(YEAR_PERCENT)
+                                    , nowDate
+                                    , mainAccount));
+                            mainAccount.setAmount(BigDecimal.ZERO);
+                        }
+                        name++;
+                    }
                 } else {
                     resheto(mainAccount, nowDate);
                 }
@@ -50,7 +67,7 @@ public class Main {
             }
             nowDate = nowDate.plusDays(1l);
             if (nowDate.getDayOfMonth() == 1) System.out.println();
-            if ((int) (mainAccount.getDeposits().stream().mapToDouble(x -> x.getAmount().doubleValue()).sum()) == 16800000)
+            if ((int) (mainAccount.getDeposits().stream().mapToDouble(x -> x.getAmount().doubleValue()).sum()) == ACC*1400000)
                 break;
             if (nowDate.getYear() > startDate.getYear() && nowDate.getMonth().getValue() == 1 && nowDate.getDayOfMonth() == 1)
                 yearStatic(mainAccount, nowDate, startDate);
